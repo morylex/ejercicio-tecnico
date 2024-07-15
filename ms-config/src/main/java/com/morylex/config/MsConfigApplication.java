ad86b165f309e0ecd9501e7c4ae0de9c59896f90
Arturo Mory Jara <morylex@hotmail.com>
2024-07-14 20:56:11 -0500

    carga inicial

---
+++ ms-config/src/main/java/com/morylex/config/MsConfigApplication.java
@@ -0,0 +1,13 @@
+package com.morylex.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConfigApplication.class, args);
+	}
+
+}
